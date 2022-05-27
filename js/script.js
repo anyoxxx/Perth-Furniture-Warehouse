73e697c4cbaa85dde2438b0bf54c044b498ba91d
Alexis Wang <alexiswang615@gmail.com>
2022-05-28 01:06:25 +0800

    fourth commit

--- js/script.js
+++ js/script.js
@@ -52,3 +52,16 @@ function prev(){
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
+
+$(document).ready(function() {
+
+    $('.color-choose input').on('click', function() {
+        var headphonesColor = $(this).attr('data-image');
+  
+        $('.active').removeClass('active');
+        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+  
+  });
+  
