1c6fa5281c416619cb55d5e44c86b2b66108f1d2
Alexis Wang <alexiswang615@gmail.com>
2022-05-30 00:54:06 +0800

    upload

--- js/script.js
+++ js/script.js
@@ -65,3 +65,10 @@ $(document).ready(function() {
   
   });
   
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+  
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
